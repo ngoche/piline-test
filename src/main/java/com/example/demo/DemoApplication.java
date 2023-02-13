88931b1fb83606826c340b98abd5bec79a66d65f
henv <ngocvanhe@gmail.com>
2023-02-14 00:38:09 +0700

    Revert "change 3"
    This reverts commit 566995c1c553018025146035a842b917b5b5e2db.

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 //change 1
-//change 2
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
