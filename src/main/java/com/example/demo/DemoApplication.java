f38a95a1fc252428afefc97ee1cb126b23be6875
henv <ngocvanhe@gmail.com>
2023-02-14 00:35:58 +0700

    revert something

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+
 //change 1
-//chang 2
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
