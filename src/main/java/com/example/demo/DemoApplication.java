95c4a5f1a04a3d391c9044f1bf7d2915ea2b2a5f
henv <ngocvanhe@gmail.com>
2023-02-14 00:33:20 +0700

    change2

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 //change 1
+//chang 2
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
